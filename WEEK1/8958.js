7ff752e95e2ea4db4df5d7868dbd7139cdc3c5cc
JinJeon <nanyong01@gmail.com>
2021-11-07 21:43:27 +0900

    WEEK1 8958 FIX

--- WEEK1/8958.js
+++ WEEK1/8958.js
@@ -6,33 +6,19 @@ const rl = readline.createInterface({
 let input = [];
 rl.on("line", function (line) {
   input.push(line);
-  console.log(input);
-  const inputArr = input.map((val) => val.split("")); // 문자를 하나씩 쪼갬
+  const inputArr = input.map((val) => val.split(""));
+  console.log(inputArr); // 문자를 하나씩 쪼갬
   const getScore = (arr) => {
     let score = 0;
-    for (i = 0; i < arr.length; i++) {
-      const route = (i) => {
+    arr.forEach((element, index) => {
+      const scoreIs = (i) => {
         if (arr[i] === "O") {
           score += 1;
-          route(i + 1);
+          scoreIs(i + 1);
         }
-        return;
       };
-      route(i);
-      // ** 반복 줄이기 **
-      // if (arr[i] === "O") {
-      //   score += 1;
-      //   if (arr[i + 1] === "O") {
-      //     score += 1;
-      //     if (arr[i + 2] === "O") {
-      //       score += 1;
-      //       if (arr[i + 3] === "O") {
-      //         score += 1;
-      //       }
-      //     }
-      //   }
-      // }
-    }
+      scoreIs(index);
+    });
     return score;
   };
   const scoreArr = inputArr.map(getScore);
