44ae71d07d8a5d9365988b365d4b38adf10fdf4c
JinJeon <nanyong01@gmail.com>
2021-11-19 15:46:51 +0900

    ⭐WEEK3 lineJump let i fix

--- WEEK3/lineJump.js
+++ WEEK3/lineJump.js
@@ -1,6 +1,6 @@
 const kangaroo = function (x1, v1, x2, v2) {
   let result = "NO";
-  let i = 1;
+  let i = 0;
   if (v1 <= v2) return result;
   while (x1 + v1 * i < x2 + v2 * i) {
     i = i + 1;
@@ -11,3 +11,5 @@ const kangaroo = function (x1, v1, x2, v2) {
   return result;
 };
 console.log(kangaroo(0, 3, 4, 2));
+console.log(kangaroo(0, 4, 4, 2));
+console.log(kangaroo(2564, 5393, 5121, 2836));
